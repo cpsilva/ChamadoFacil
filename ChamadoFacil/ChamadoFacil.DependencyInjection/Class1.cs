f2c74cb8d0f59cfc77ce1108dc074c4813a3a3a7
cpsilva <caiopirees@gmail.com>
2018-05-13 21:54:23 -0300

    Criado divisão das camadas do projeto

---
+++ ChamadoFacil/ChamadoFacil.DependencyInjection/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChamadoFacil.DependencyInjection
+{
+    public class Class1
+    {
+    }
+}
